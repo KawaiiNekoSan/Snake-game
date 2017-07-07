b9eb251f8527dd129ca51110a8b1d6bb1fbabacd
KawaiNekoSan <beneficeromain@gmail.com>
2017-07-07 23:48:12 +0200

    First commit - fix bug and add speed increment
    First commit - fix bug and add speed increment

--- js/app.js
+++ js/app.js
@@ -5,40 +5,40 @@ btn.addEventListener("click", function(){ drawModule.init();});
 
 	document.onkeydown = function(event) {
 
-        keyCode = window.event.keyCode; 
-        keyCode = event.keyCode;
+	  keyCode = event.keyCode;
 
-        switch(keyCode) {
-        
-        case 37: 
-          if (direction != 'right') {
-            direction = 'left';
-          }
-          console.log('left'); 
-          break;
+	  switch(keyCode) {
 
-        case 39:
-          if (direction != 'left') {
-          direction = 'right';
-          console.log('right');
-          }
-          break;
+	  case 37:
+	    if (direction != 'right') {
+	    	direction = 'left';
+				console.log(direction);
+	    }
+			break;
 
-        case 38:
-          if (direction != 'down') {
-          direction = 'up';
-          console.log('up');
-          }
-          break;
+	  case 39:
+	    if (direction != 'left') {
+	    	direction = 'right';
+	    	console.log(direction);
+	    }
+		 	break;
 
-        case 40:
-          if (direction != 'up') {
-          direction = 'down';
-          console.log('down');
-          }
-          break;
-          }
-      }
+	  case 38:
+	    if (direction != 'down') {
+	    	direction = 'up';
+	    	console.log(direction);
+			}
+			break;
+
+	  case 40:
+	    if (direction != 'up') {
+	    	direction = 'down';
+	    	console.log('down');
+	    }
+			break;
+
+	  }
+  }
 
 
 })(window, document, drawModule);
--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,5 @@
 
-var drawModule = (function () { 
+var drawModule = (function () {
 
   var bodySnake = function(x, y) {
         ctx.fillStyle = 'green';
@@ -26,9 +26,9 @@ var drawModule = (function () {
       snake = [];
       for (var i = length-1; i>=0; i--) {
           snake.push({x:i, y:0});
-      }  
+      }
   }
-    
+
   var paint = function(){
       ctx.fillStyle = 'lightgrey';
       ctx.fillRect(0, 0, w, h);
@@ -40,14 +40,18 @@ var drawModule = (function () {
       var snakeX = snake[0].x;
       var snakeY = snake[0].y;
 
-      if (direction == 'right') { 
-        snakeX++; }
-      else if (direction == 'left') { 
-        snakeX--; }
-      else if (direction == 'up') { 
-        snakeY--; 
-      } else if(direction == 'down') { 
-        snakeY++; }
+      if (direction == 'right') {
+        snakeX++;
+      }
+      else if (direction == 'left') {
+        snakeX--;
+      }
+      else if (direction == 'up') {
+        snakeY--;
+      }
+      else if(direction == 'down') {
+        snakeY++;
+      }
 
       if (snakeX == -1 || snakeX == w/snakeSize || snakeY == -1 || snakeY == h/snakeSize || checkCollision(snakeX, snakeY, snake)) {
           //restart game
@@ -55,17 +59,19 @@ var drawModule = (function () {
 
           ctx.clearRect(0,0,w,h);
           gameloop = clearInterval(gameloop);
-          return;          
+          return;
         }
-        
+
+        var isAppleEaten = false;
         if(snakeX == food.x && snakeY == food.y) {
+          isAppleEaten = true;
           var tail = {x: snakeX, y: snakeY}; //Create a new head instead of moving the tail
           score ++;
-          
           createFood(); //Create new food
-        } else {
+        }
+        else {
           var tail = snake.pop(); //pops out the last cell
-          tail.x = snakeX; 
+          tail.x = snakeX;
           tail.y = snakeY;
         }
         //The snake can now eat the food.
@@ -73,10 +79,23 @@ var drawModule = (function () {
 
         for(var i = 0; i < snake.length; i++) {
           bodySnake(snake[i].x, snake[i].y);
-        } 
-        
-        pizza(food.x, food.y); 
+        }
+
+        pizza(food.x, food.y);
         scoreText();
+
+        //increment speed
+        if(isAppleEaten){
+          let speed;
+          if(currentSpeed - 5 > maxSpeed){
+            speed = currentSpeed-=5;
+          }
+          else {
+            speed = currentSpeed = maxSpeed;
+          }
+          gameloop = clearInterval(gameloop);
+          gameloop = setInterval(paint, speed);
+        }
   }
 
   var createFood = function() {
@@ -88,7 +107,7 @@ var drawModule = (function () {
       for (var i=0; i>snake.length; i++) {
         var snakeX = snake[i].x;
         var snakeY = snake[i].y;
-      
+
         if (food.x===snakeX && food.y === snakeY || food.y === snakeY && food.x===snakeX) {
           food.x = Math.floor((Math.random() * 30) + 1);
           food.y = Math.floor((Math.random() * 30) + 1);
@@ -99,16 +118,19 @@ var drawModule = (function () {
   var checkCollision = function(x, y, array) {
       for(var i = 0; i < array.length; i++) {
         if(array[i].x === x && array[i].y === y)
-        return true;
-      } 
+          return true;
+      }
       return false;
   }
 
   var init = function(){
+      initSpeed = 80;
+      maxSpeed = 15;
+      currentSpeed = initSpeed;
       direction = 'down';
       drawSnake();
       createFood();
-      gameloop = setInterval(paint, 80);
+      gameloop = setInterval(paint, initSpeed);
   }
 
 
@@ -116,5 +138,5 @@ var drawModule = (function () {
       init : init
     };
 
-    
+
 }());
